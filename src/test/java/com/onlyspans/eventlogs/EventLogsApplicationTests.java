503b1ce62b8ba86dc43bc12b473f0ead7f339e38
nikitakaralius <karalyus@mindbox.cloud>
2025-12-06 15:00:27 +0300

    Initialized project from Spring Initializr

---
+++ src/test/java/com/onlyspans/eventlogs/EventLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlyspans.eventlogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
